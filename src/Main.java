d29e834fe2a5fdec7bd3cc1cf41b09dc1ba263ea
BGennady <bg.korolew@gmail.com>
2024-10-16 00:12:30 +0400

    Update Main2.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
-public class Main {
+public class UpdatedMain {
     // создаем потокобезопасную мапу для хранения "ключ" - "значение";
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    public static final Object lock = new Object();
 
 
     public static void main(String[] args) throws InterruptedException {
@@ -12,6 +13,29 @@ public class Main {
         List<Thread> threads = new ArrayList<>();
 
         char charToCount = 'R';
+
+        // создаем поток для вывода текущего максимума;
+
+        Thread maxPrinter = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (lock) {
+                    try {
+                        lock.wait();// ждем сигнала
+                    } catch (InterruptedException e) {
+                        break; // выход из цикла при прерывании
+                    }
+
+                    // проверяем не пустая ли мапа
+                    if (!sizeToFreq.isEmpty()) {
+                        int liderKey = Collections.max(sizeToFreq.entrySet(),
+                                Map.Entry.comparingByValue()).getKey();
+                        int liderFrequency = sizeToFreq.get(liderKey);
+                        System.out.printf("Текущий лидер %d (встретилось %d раз)\n", liderKey, liderFrequency);
+                    }
+                }
+            }
+        });
+        maxPrinter.start();
 
         // создаем потоки;
         for (int i = 0; i < numThreads; i++) {
@@ -25,29 +49,36 @@ public class Main {
                     // проверяем Мапу по ключу rCount и если находит то увеличивает колличество повторов на +1;
                     sizeToFreq.put(rCount, sizeToFreq.getOrDefault(rCount, 0) + 1);
                 }
+                synchronized (lock) {
+                    lock.notify();
+                }
             });
             threads.add(thread);
             thread.start();
         }
+
         // дожидаемся завершения всех потоков
         for (Thread thread : threads) {
             thread.join();
         }
 
+        // прерываем поток вывода максимума
+        maxPrinter.interrupt();
+
         // получаем ключ мах частоты-значения;
-        int maxSize = Collections.max(sizeToFreq.entrySet(),
+        int finalMaxSize = Collections.max(sizeToFreq.entrySet(),
                 Map.Entry.comparingByValue()).getKey();
         // получаем мах значение по ключу;
-        int maxFrequency = sizeToFreq.get(maxSize);
+        int finalMaxFrequency = sizeToFreq.get(finalMaxSize);
 
         System.out.printf("Самое частое количество повторений %d (встретилось %d раз) " +
-                "\n другие размеры:\n", maxSize, maxFrequency);
+                "\n другие размеры:\n", finalMaxSize, finalMaxFrequency);
 
         // в entry  кладем пару ключ значение;
         for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
             int size = entry.getKey(); // получаем ключ;
             int frequency = entry.getValue(); // получаем значение;
-            if (size != maxSize) {
+            if (size != finalMaxSize) {
                 // вывод частот для остальных ключей;
                 System.out.printf("- %d (%d раз) \n", size, frequency);
             }
